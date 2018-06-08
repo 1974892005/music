6292ca998cb40a1a91b076f0ac589704b34aeec7
彬Gg <1974892005@qq.com>
2018-06-08 22:03:06 +0800

    about

---
+++ about.php
@@ -0,0 +1,20 @@
+﻿<?php
+include_once 'header.inc.php';
+?>
+
+
+
+
+    <div class="jumbotron">
+        <h4>暂不支持</h4>
+
+            <p class="lead ">
+ <a  href="http://www.dibin.xyz/wz">请访问短链接服务</a></p>
+
+
+    </div>
+
+
+<?php
+include_once 'footer.inc.php';
+?>
